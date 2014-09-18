8e3287a70f99d95255d32a4292ee742b1d484fed
Thomas Boerger <tboerger@suse.de>
2014-09-18 12:46:27 +0200

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: mpd
+# Attributes:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["mpd"]["packages"] = %w(
+  mpd
+  mpc
+  ncmpc
+)
+
+default["mpd"]["zypper"]["alias"] = "packman-multimedia"
+default["mpd"]["zypper"]["title"] = "Packman Multimedia"
+default["mpd"]["zypper"]["repo"] = "http://packman.inode.at/suse/openSUSE_#{node["platform_version"]}/Multimedia/"
+default["mpd"]["zypper"]["key"] = "#{node["mpd"]["zypper"]["repo"]}repodata/repomd.xml.key"
